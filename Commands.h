d01e1c772866dacf8a089d2daff574dc09cf8f9d
Borat <ethanbarbar@gmail.com>
2020-11-16 10:39:13 +0200

    Finished signals and commands regarding signals, probably there are errors hiding.

--- Commands.h
+++ Commands.h
@@ -18,11 +18,16 @@ private:
 // TODO: Add your data members
 public:
     SmallShell *shell;
-    const char *cmd_line;
-    const char *original_cmd_line;
+
+    char *cmd_line;
+    char *original_cmd_line;
     bool should_operate;
 
     Command(const char *cmd_line);
+
+    int getMaxJobID(char *const *args) const;
+
+    int GetMaxStoppedJobID(char *const *args) const;
 
     virtual ~Command();
 
@@ -154,29 +159,31 @@ public:
 
 };
 
+class JobEntry {
+public:
+    Command *command;
+    time_t start_time;
+    int ID;
+    int pid;
+    JobStatus status;
+
+    JobEntry(Command *cmd, int id, int processID, JobStatus status) {
+        this->command = cmd;
+        this->ID = id;
+        this->pid = processID;
+        this->status = status;
+        start_time = time(nullptr);
+        // TODO: Add start time.
+    }
+};
+
 class JobsList {
 private:
 
 public:
-    class JobEntry {
-    public:
-        Command *command;
-        time_t start_time;
-        int ID;
-        int processID;
-        JobStatus status;
-
-        JobEntry(Command *cmd, int id, int processID, JobStatus status) {
-            this->command = cmd;
-            this->ID = id;
-            this->processID = processID;
-            this->status = status;
-            start_time = time(nullptr);
-            // TODO: Add start time.
-        }
-    };
 
     std::vector<JobEntry *> jobs;
+    JobEntry *currentJob;
     int counter;
     // TODO: Add your data members
 
@@ -185,6 +192,10 @@ public:
     ~JobsList() {}
 
     void addJob(Command *cmd, int processID, bool isStopped = false);
+
+    void setCurrentJob(Command *cmd, int processID, bool isStopped = false) {
+        this->currentJob = new JobEntry(cmd, -1, processID, Foreground);
+    }
 
     void printJobsList();
 
@@ -211,7 +222,7 @@ public:
 class JobsCommand : public BuiltInCommand {
     // TODO: Add your data members
 public:
-    JobsCommand(const char *cmd_line, JobsList *jobs);
+    JobsCommand(const char *cmd_line);
 
     virtual ~JobsCommand() {}
 
@@ -221,11 +232,14 @@ public:
 class KillCommand : public BuiltInCommand {
     // TODO: Add your data members
 public:
-    KillCommand(const char *cmd_line, JobsList *jobs);
+    KillCommand(const char *cmd_line);
 
     virtual ~KillCommand() {}
 
     void execute() override;
+
+    int jobPID;
+    int signum;
 };
 
 class ForegroundCommand : public BuiltInCommand {
@@ -233,10 +247,9 @@ class ForegroundCommand : public BuiltInCommand {
 public:
     int jobID;
 
-    ForegroundCommand(const char *cmd_line, JobsList *jobs);
+    ForegroundCommand(const char *cmd_line);
 
     virtual ~ForegroundCommand() {}
-    int GetJobID(char *const *args) const;
 
     void execute() override;
 
@@ -247,8 +260,8 @@ class BackgroundCommand : public BuiltInCommand {
 public:
     int jobID;
 
-    BackgroundCommand(const char *cmd_line, JobsList *jobs);
-    int GetJobID(char *const *args) const;
+    BackgroundCommand(const char *cmd_line);
+
 
     virtual ~BackgroundCommand() {}
 
--- signals.cpp
+++ signals.cpp
@@ -6,14 +6,35 @@
 using namespace std;
 
 void ctrlZHandler(int sig_num) {
-	// TODO: Add your implementation
+    cout << "smash: got ctrl-Z\n";
+    auto shell = &SmallShell::getInstance();
+    auto currentJob = shell->jobsList.currentJob;
+    if (currentJob != nullptr) {
+
+        cout << "smash: process " << currentJob->pid << " was stopped\n";
+        currentJob->status = Stopped;
+        shell->jobsList.addJob(currentJob->command, currentJob->pid, true);
+        // Shit for memory management.
+        currentJob->command = nullptr;
+        delete currentJob;
+        shell->jobsList.currentJob = nullptr;
+        kill(currentJob->pid, SIGSTOP);
+    }
 }
 
 void ctrlCHandler(int sig_num) {
-  // TODO: Add your implementation
+    cout << "smash: got ctrl-C\n";
+    auto shell = &SmallShell::getInstance();
+    auto currentJob = shell->jobsList.currentJob;
+    if (currentJob != nullptr) {
+        cout << "smash: process " << currentJob->pid << " was killed\n";
+        kill(currentJob->pid, SIGKILL);
+        delete currentJob;
+        shell->jobsList.currentJob = nullptr;
+    }
 }
 
 void alarmHandler(int sig_num) {
-  // TODO: Add your implementation
+    // TODO: Add your implementation
 }
 
